9c03382b8d0490746aa0974a77d4324fb4f892b1
iamsanjaysingh <iamsanjaypratapsingh@gmail.com>
2020-03-26 17:46:10 +0530

    header

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,3 +1,18 @@
 $(document).ready(function (){
 
+    //Including Includes Assets
+    $('#header').load('includes/header.html');
+    if($('#header').length){$('head').append($('<link rel="stylesheet" type="text/css"/>').attr('href','assets/css/header.css'));}
+    $('#footer').load('includes/footer.html');
+    if($('#footer').length){$('head').append($('<link rel="stylesheet" type="text/css"/>').attr('href','assets/css/footer.css'));}
+
+    //Pagepiling
+    $('#pagepiling').pagepiling({
+    });
+
+    //Header
+    $(document).on('click','#headerToggle',function(){
+        $(this).parents('#header').toggleClass('active');
+    });
+
 });
